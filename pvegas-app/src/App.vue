cc5e2991b58681e7d2d34b6d2d14e9d11bc51e14
Juan Moreno <56707302+morenj@users.noreply.github.com>
2024-02-22 10:17:37 -0800

    Initial commit

---
+++ pvegas-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Home msg="HELLO WORLD!"/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+body {
+  background-color: black;
+}
+</style>
